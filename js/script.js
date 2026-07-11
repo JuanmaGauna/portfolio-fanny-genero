3d8238c50ae917775494b7f0281a098607b9637f
Juan Manuel Gauna <juanmanuelgauna@gmail.com>
2026-07-11 17:50:22 -0300

    Primer commit - estructura inicial del portfolio

---
+++ js/script.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", () => {
+
+    const preloader = document.getElementById("preloader");
+
+    if (preloader) {
+        preloader.style.opacity = "0";
+
+        setTimeout(() => {
+            preloader.style.display = "none";
+        }, 500);
+    }
+
+});
