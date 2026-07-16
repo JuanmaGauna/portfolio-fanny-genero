b53140fc158e1dda0ed1a486eba5bb1ca210e781
Juan Manuel Gauna <juanmanuelgauna@gmail.com>
2026-07-16 09:33:43 -0300

    FOOTER completo

--- js/script.js
+++ js/script.js
@@ -42,3 +42,33 @@ AOS.init({
     once:true
 
 });
+/*==================================================
+                BACK TO TOP
+==================================================*/
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 500){
+
+        backToTop.classList.add("show");
+
+    }else{
+
+        backToTop.classList.remove("show");
+
+    }
+
+});
+
+backToTop.addEventListener("click", () => {
+
+    window.scrollTo({
+
+        top:0,
+
+        behavior:"smooth"
+
+    });
+
+});
